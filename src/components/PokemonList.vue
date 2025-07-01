325f21318e65d31e0b17648a83def54c38edaa6b
khanhlinh-jnf <ntklinh0704@gmail.com>
2025-07-01 22:25:27 +0700

    Pagination

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,30 +1,40 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import PokemonCard from './PokemonCard.vue';
 
 const pokemons = ref<{ name: string; url: string }[]>([])
 const loading = ref(true)
 const error = ref('')
+const currentUrl = ref('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+const nextUrl = ref<string | null>(null)
+const prevUrl = ref<string | null>(null)
+
 
 const fetchPokemons = async () => {
   try {
     loading.value = true
     error.value = ''
 
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+    const response = await fetch(currentUrl.value)
     const data = await response.json()
 
     pokemons.value = data.results
+    nextUrl.value = data.next
+    prevUrl.value = data.previous
   } catch (err) {
-    error.value = 'Không thể tải dữ liệu Pokémon!'
+    error.value = 'Không thể tải dữ liệu Pokemon!'
     console.error(err)
   } finally {
     loading.value = false
   }
 }
 
+watch(currentUrl, () => {
+  fetchPokemons()
+})
+
 const handleSelect = (pokemon: any) => {
-  console.log('Pokémon được chọn:', pokemon.name)
+  console.log('Pokemon được chọn:', pokemon.name)
 }
 
 onMounted(() => {
@@ -34,7 +44,7 @@ onMounted(() => {
 
 <template>
   <div class="pokemon-list">
-    <p v-if="loading">Đang tải danh sách Pokémon...</p>
+    <p v-if="loading">Đang tải danh sách Pokemon...</p>
     <p v-if="error" class="error">{{ error }}</p>
 
     <div class="grid">
@@ -45,18 +55,41 @@ onMounted(() => {
         @select="handleSelect"
       />
     </div>
+
+    <div class="pagination">
+      <button :disabled="!prevUrl || loading" @click="currentUrl = prevUrl || currentUrl">Previous</button>
+      <button :disabled="!nextUrl || loading" @click="currentUrl = nextUrl || currentUrl">Next</button>
+      </div>
   </div>
 </template>
 
 <style scoped>
-
 .grid {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 }
-
-.error {
-  color: red;
+.pagination {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+}
+button {
+  padding: 8px 16px;
+  border: none;
+  border-radius: 6px;
+  background-color: #3b82f6;
+  color: white;
+  font-weight: 500;
+  cursor: pointer;
+  transition: 0.2s;
+}
+button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+button:hover:not(:disabled) {
+  background-color: #2563eb;
 }
 </style>
